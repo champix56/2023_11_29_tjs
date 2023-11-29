72a27b9bddf78424efa9e3b731b60881e410ba4f
champix56 <desorbaix@free.fr>
2023-11-29 14:15:26 +0100

    2eme syntaxe

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -10,8 +10,8 @@ function App() {
       <Button text="toto" type="button"/>
       <Button text="tata" type="submit"/>
       <Button text="titi" type="reset"/>
-      <Button text="titi" />
-    </div>
+      <Button text="titi"  />
+    </div> 
   );
 }
 
