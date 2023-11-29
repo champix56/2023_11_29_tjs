57a9ceef1004e00801b63708d3474326e9a07f3c
champix56 <desorbaix@free.fr>
2023-11-29 12:26:48 +0100

    css de base

--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
+import './Button.css'
 function Button(props) {
-    return <button>Benjamin</button>
+    return <button className='Button'>Benjamin</button>
 }
 export default Button;
