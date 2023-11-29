44deaa0a0f28549fcaa3d20b4431ce438ccc9811
champix56 <desorbaix@free.fr>
2023-11-29 12:15:35 +0100

    1 er composant

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
-    <div className="App">DEMAT BREIZH</div>
+    <div className="App">DEMAT BREIZH
+      <hr/>
+      <Button/>
+    </div>
   );
 }
 
