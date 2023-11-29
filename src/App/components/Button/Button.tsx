f460405015e2d1e620d251eb8ebc38084d7a5eda
champix56 <desorbaix@free.fr>
2023-11-29 14:14:29 +0100

    demo en ts avec type interface pour props

--- src/App/components/Button/Button.tsx
+++ src/App/components/Button/Button.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from './Button.module.css'
-function Button(props) {
+import { Interface } from 'readline'
+interface IButtonProps {
+    type?:'button'|'submit'|'reset'|undefined
+    text:string
+}
+function Button(props:IButtonProps) {
     console.log(props)
     //afect impossible --> props.text+='coucou'
     return <button type={props.type} className={styles.Button}>{props.text}</button>
