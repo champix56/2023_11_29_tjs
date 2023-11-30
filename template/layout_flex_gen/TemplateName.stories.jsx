25f7cb9b16b5009c2b9601d8acae6c507b7696e4
champix56 <desorbaix@free.fr>
2023-11-30 10:19:03 +0100

    template pour layout

---
+++ template/layout_flex_gen/TemplateName.stories.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import TemplateName from "./TemplateName";
+
+export default {
+  title: "TemplateName",
+};
+
+export const Default = () => (
+  <TemplateName>
+    <div>coucou</div>
+    <div>coucou</div>
+  </TemplateName>
+);
+
+Default.story = {
+  name: "default",
+};
