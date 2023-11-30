3f2c0cdf6486b263716235c2fa8c1f1608a81ace
champix56 <desorbaix@free.fr>
2023-11-30 13:50:18 +0100

    memeSvgViewer base

---
+++ src/App/components/uis/MemeSvgViewer/MemeSvgViewer.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import MemeSvgViewer from './MemeSvgViewer';
+import {emptyMeme} from 'orsys-tjs-meme'
+describe('<MemeSvgViewer />', () => {
+  test('it should mount', () => {
+    render(<MemeSvgViewer meme={emptyMeme} />);
+    
+    const memeSvgViewer = screen.getByTestId('MemeSvgViewer');
+
+    expect(memeSvgViewer).toBeInTheDocument();
+  });
+});
