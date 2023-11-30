db74add3e843327a1e2522253d53b6baf1d4e76e
champix56 <desorbaix@free.fr>
2023-11-30 14:05:53 +0100

    ajout d'etat pour le meme courrant

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./components/uis/Footer/Footer";
 import Navbar from "./components/uis/Navbar/Navbar";
@@ -7,15 +7,17 @@ import FlexHGrow3 from "./components/layouts/FlexHGrow3/FlexHGrow3";
 import FlexWGrow1 from "./components/layouts/FlexWGrow1/FlexWGrow1";
 import MemeSvgViewer from "./components/uis/MemeSvgViewer/MemeSvgViewer";
 import MemeForm from "./components/functionnal/MemeForm/MemeForm";
+import {emptyMeme} from 'orsys-tjs-meme'
 const App = () => {
+  const [current, setcurrent] = useState(emptyMeme)
   return (
-    <div className="App">
+    <div className="App" data-testid="App">
       <FlexHGrow3 style={{height:'calc(100vh - 30px)'}}>
         <Header />
         <Navbar />
         <FlexWGrow1>
-          <MemeSvgViewer />
-          <MemeForm />
+          <MemeSvgViewer meme={current} />
+          <MemeForm meme={current}/>
         </FlexWGrow1>
         <Footer />
       </FlexHGrow3>
