6963fee0fb43d7e3b05f8b751396a6a4af1dd1ca
champix56 <desorbaix@free.fr>
2023-11-30 15:32:40 +0100

    fin de gestion avec state intermediaire de form

--- src/App/components/functionnal/MemeForm/MemeForm.jsx
+++ src/App/components/functionnal/MemeForm/MemeForm.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./MemeForm.module.css";
 import Button from "../../uis/Button/Button";
 
 const MemeForm = (props) => {
+  const [state, setstate] = useState(props.meme);
+  useEffect(() => {
+    setstate(props.meme);
+  }, [props]);
+
+  const manageStringChangeEvent = (evt) => {
+    let newState = { ...state };
+    newState[evt.target.name] = evt.target.value;
+    setstate(newState);
+  };
+  const manageNumberChangeEvent = (evt) => {
+    let newState = { ...state };
+    newState[evt.target.name] = Number(evt.target.value);
+    setstate(newState);
+  };
   return (
     <div className={styles.MemeForm} data-testid="MemeForm">
-      <form>
-        <label for="titre">
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          props.onMemeChange(state);
+        }}
+        onReset={(evt) => {
+          setstate(props.meme);
+        }}
+      >
+        <label htmlFor="titre">
           <h1>Titre</h1>
         </label>
         <br />
-        <input name="titre" id="titre" />
+        <input
+          name="titre"
+          id="titre"
+          value={state.titre}
+          onChange={manageStringChangeEvent}
+        />
         <hr />
-        <label for="image">
+        <label htmlFor="image">
           <h2>Image</h2>
         </label>
         <br />
@@ -24,29 +52,55 @@ const MemeForm = (props) => {
           <option value="4">gwenadu.jpg</option>
         </select>
         <hr />
-        <label for="text">
+        <label htmlFor="text">
           <h2>texte</h2>
         </label>
         <br />
-        <input name="text" id="text" type="text" />
+        <input
+          name="text"
+          id="text"
+          type="text"
+          value={state.text}
+          onChange={manageStringChangeEvent}
+        />
         <br />
-        <label for="x">
+        <label htmlFor="x">
           <h2 className={styles.inline}>x :</h2>
         </label>
-        <input className={styles.smallNumber} name="x" id="x" type="number" />
-        <label for="y">
+        <input
+          className={styles.smallNumber}
+          name="x"
+          id="x"
+          type="number"
+          value={state.x}
+          onChange={manageNumberChangeEvent}
+        />
+        <label htmlFor="y">
           <h2 className={styles.inline}>y :</h2>
         </label>
-        <input className={styles.smallNumber} name="y" id="y" type="number" />
+        <input
+          className={styles.smallNumber}
+          name="y"
+          id="y"
+          type="number"
+          value={state.y}
+          onChange={manageNumberChangeEvent}
+        />
         <hr />
         <br />
         <h2>Decorations</h2>
-        <label for="color">
+        <label htmlFor="color">
           <h2 className={styles.inline}>color :</h2>
         </label>
-        <input name="color" id="color" type="color" />
+        <input
+          name="color"
+          id="color"
+          type="color"
+          value={state.color}
+          onChange={manageStringChangeEvent}
+        />
         <br />
-        <label for="fontSize">
+        <label htmlFor="fontSize">
           <h2 className={styles.inline}>font-size :</h2>
         </label>
         <input
@@ -55,10 +109,12 @@ const MemeForm = (props) => {
           id="fontSize"
           type="number"
           min="0"
+          value={state.fontSize}
+          onChange={manageNumberChangeEvent}
         />
         px
         <br />
-        <label for="fontWeight">
+        <label htmlFor="fontWeight">
           <h2 className={styles.inline}>font-weight :</h2>
         </label>
         <input
@@ -69,22 +125,40 @@ const MemeForm = (props) => {
           min="100"
           step="100"
           max="900"
+          value={state.fontWeight}
+          onChange={manageStringChangeEvent}
         />
         <br />
-        <input name="underline" id="underline" type="checkbox" />
+        <input
+          name="underline"
+          id="underline"
+          type="checkbox"
+          checked={state.underline}
+          onChange={(evt) => {
+            setstate({ ...state, underline: evt.target.checked });
+          }}
+        />
         &nbsp;
-        <label for="underline">
+        <label htmlFor="underline">
           <h2 className={styles.inline}>underline</h2>
         </label>
         &nbsp;<h2 className={styles.inline}>/</h2>&nbsp;
-        <label for="italic">
+        <label htmlFor="italic">
           <h2 className={styles.inline}>italic</h2>
         </label>
         &nbsp;
-        <input name="italic" id="italic" type="checkbox" />
+        <input
+          name="italic"
+          id="italic"
+          type="checkbox"
+          checked={state.italic}
+          onChange={(evt) => {
+            setstate({ ...state, italic: evt.target.checked });
+          }}
+        />
         <hr />
         <br />
-        <label for="frameSizeX">
+        <label htmlFor="frameSizeX">
           <h2 className={styles.inline}>frame size X :</h2>
         </label>
         <input
@@ -95,7 +169,7 @@ const MemeForm = (props) => {
           min="0"
         />
         px{" "}
-        <label for="frameSizeY">
+        <label htmlFor="frameSizeY">
           <h2 className={styles.inline}>frame size y :</h2>
         </label>
         <input
@@ -107,7 +181,6 @@ const MemeForm = (props) => {
         />
         px
         <br />
-        <hr/>
         <div className={styles.flexCols2}>
           <Button type={"reset"} bgColor={"tomato"}>
             Cancel
