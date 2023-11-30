c6abdf9df501fd79fa7743f7d763d281849615e5
champix56 <desorbaix@free.fr>
2023-11-30 11:36:02 +0100

    header

---
+++ src/App/components/uis/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Header.module.css';
+
+const Header = () => (
+  <div className={styles.Header} data-testid="Header">
+    <span className={styles.meme}>Meme</span>.<span className={styles.js}>js</span>
+  </div>
+);
+export default Header
