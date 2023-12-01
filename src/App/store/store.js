de561cb794bcee75dacddbc9dced3b92f7c1042f
champix56 <desorbaix@free.fr>
2023-12-01 11:16:13 +0100

    config du store basique

---
+++ src/App/store/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import ressourceReducer, { fillImages } from './ressources'
+
+//export const store = createStore(ressourceReducer)
+export const store = configureStore({
+    reducer: ressourceReducer,
+    devTools: true
+})
+store.subscribe(()=>console.trace(store.getState()))
+console.log('chargement du store')
+console.log(store.getState())
+// console.log(fillImages([{ id: 0 }, { id: 1 }]))
+
+//store.dispatch({ type: 'ressources/fillImages', payload: [{ id: 0 }] })
+/* store.dispatch(fillImages([{ id: 0 }]))
+
+store.dispatch(fillImages([{ id: 0 }]))*/
